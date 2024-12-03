a7f1af5a1db9c0e141417a9032161a4f4172aca9
Yanuko0 <https://github.com/Yanuko0>
2024-12-03 19:32:18 +0800

    準備部署到 Vercel

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  basePath: '/draw-chat',  // 類似於 Vite 的 base 配置
-  output: 'export',            // 用於靜態輸出
+  basePath: '/draw-chat',  // 類似於 Vite 的 base 配置 
   distDir: 'docs',            // 類似於 Vite 的 outDir 配置
   images: {
     unoptimized: true,
